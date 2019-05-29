9e0a30ff0fe75fe9349049e02c25233c0f062e62
Rafael Urdaneta <rafael.urdaneta@ccorreo.unimet.edu.ve>
2019-05-28 20:00:12 -0400

    ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.render('index', { title: 'Hey', message: 'TU CINE BEBE'});
+    res.render('index.pug', { title: 'Hey', message: 'TU CINE BEBE'});
 })
 
 router.get('/Sedes', (req, res)=>{
-    res.render('Sedes', { title: 'Hey', message: 'Sedes'});
+    res.render('Sedes.pug', { title: 'Hey', message: 'Sedes'});
 })
 
 router.get('*', (req, res)=>{
