451bad2fb303913c4691ab86605c87f0e0f35a52
Jorgejpc <jorgepc9531@gmail.com>
2019-06-05 16:17:00 -0400

    ajaaa

--- database/db.sql
+++ database/db.sql
@@ -3,12 +3,16 @@ CREATE DATABASE database_proyect;
 USE database_proyect;
 
 CREATE TABLE Sede(
-    nombreFiscal VARCHAR() NOT NULL,
-    locacion VARCHAR() NOT NULL
+    nombreFiscal INT(10) NOT NULL,
+    ubicacion VARCHAR(60) NOT NULL,
+    horario DATETIME(6) NOT NULL
 );
 
 
 ALTER TABLE Sede
     ADD PRIMARY KEY (nombreFiscal);
+
+ALTER TABLE Sede
+    MODIFY nombreFiscal INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE Sede;
